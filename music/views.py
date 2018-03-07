7b6c277ae20f549d25c28e7c2a6bf2df05876b3c
orache arthur <arthur.orache@gmail.com>
2018-03-07 14:49:32 +0300

    Initial commit

--- music/views.py
+++ music/views.py
@@ -102,24 +102,5 @@ class UserRegView(View):
             })
             user.email_user(subject, message)
             return redirect('music:account_activation_sent')
-       
+
         return render(request, self.template_name, {'form': form})
-
-def account_activation_sent(request):
-    return render(request, 'account_activation_sent.html')
-
-def activate(request, uidb64, token):
-    try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
-
-    if user is not None and account_activation_token.check_token(user, token):
-        user.is_active = True
-        user.profile.email_confirmed = True
-        user.save()
-        login(request, user)
-        return redirect('home')
-    else:
-        return render(request, 'account_activation_invalid.html')
