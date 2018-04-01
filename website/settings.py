ef6d71f8ccaf691680265e370bcaab1243b10365
orache arthur <arthur.orache@gmail.com>
2018-04-01 15:13:48 +0300

    Fix settings for heroku

--- website/settings.py
+++ website/settings.py
@@ -134,11 +134,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
-
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
